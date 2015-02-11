3d1dd7c9729fb5979064d3be404350ef0044a28f
Francesc Pinyol Margalef <francesc.pinyol.m@gmail.com>
2015-02-11 12:55:41 +0100

    django 1.7: customized admin headers

--- Simple Django client/app/urls.py
+++ Simple Django client/app/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.views.generic import RedirectView
 
+from django.utils.translation import ugettext_lazy as _
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -31,4 +32,8 @@ else:
 	
 	    (r'^' + settings.MEDIA_URL[1:] + '(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),  # Never use this in prod !
 	)
-	
+
+# for Django >= 1.7
+admin.site.site_header = _('PlugIt administration') # "Django administration"
+admin.site.site_title = _('PlugIt site admin') # "Django site admin"
+admin.site.index_title = _('Site administration') # "Site administration"
