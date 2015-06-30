bbcdc0954ad57abd3ee83e55b3114c4d3e0cdb36
barroco <barroco@ebu.ch>
2015-06-30 13:50:03 +0200

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+    name = "plugit",
+    packages = ["plugit"],
+    version = "0.0.1b",
+    license = "BSD",
+    description = "PlugIt is a framework enhancing the portability and integration of web services requiring a user interface.",
+    author = "EBU Technology & Innovation",
+    author_email = "barroco@ebu.ch",
+    url = "http://github.com/ebu/PlugIt",
+    download_url = "",
+    keywords = ["frontend"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Flask",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        ],
+    long_description = """\
+PlugIt
+-------------------------------------
+
+PlugIt is a framework enhancing the portability and integration of web services requiring a user interface.
+
+"""
+)
