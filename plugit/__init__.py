6c0808c3ebd2de5f6f429dd21ef1e45b0d3af6ae
Maximilien Cuony <maximilien@theglu.org>
2017-05-04 17:16:26 +0200

    Use format for string

--- plugit/__init__.py
+++ plugit/__init__.py
@@ -6,7 +6,7 @@ import routes
 from params import PI_BASE_URL
 
 
-app = Flask(getattr(config, 'PI_PROJECT_NAME', 'PlugIT-Project'), static_folder='media', static_url_path=PI_BASE_URL+'media')
+app = Flask(getattr(config, 'PI_PROJECT_NAME', 'PlugIT-Project'), static_folder='media', static_url_path='{}media'.format(PI_BASE_URL))
 
 
 def load_actions(act_mod, mail_callback=None):
